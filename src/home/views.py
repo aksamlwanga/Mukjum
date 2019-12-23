ae6ac5012717dce53a0c3f3c1de0fc5617c518ac
aksamlwanga <lwangaaksam@gmail.com>
2019-12-24 01:36:46 +0300

    template fully working ,we should start working upon the project

---
+++ src/home/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+
+def indexview(request):
+    return render(request,"index.html")
+
+def checkOutView(request):
+    return render(request,"check-out.html")
+
+def contactView(request):
+    return render(request,"contact.html")
+
+def categoriesView(request):
+    return render(request,"categories.html")
+
+def shoppingCartView(request):
+    return render(request,"shopping-cart.html")
+
+def productPageView(request):
+    return render(request,"product-page.html")
+
+# def register(request):
+#     if request.method=="POST":
+#         # form=UserCreationForm(request.POST)
+#         form=UserRegistrationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username=form.cleaned_data.get('username')
+#             messages.success(request,f'Your account has been created! You are now able to log in ')
+#             return redirect('')
+#     else:
+#         # form=UserCreationForm()
+#          form=UserRegistrationForm()
+#     return render(request,'register.html',{'form':form})
+
+
+
